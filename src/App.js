af5928c1362d1d338d65c9855d1128a88b2187e2
FFeng <fsfeng@users.noreply.github.com>
2021-01-20 17:25:47 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
+import { Helmet } from "react-helmet";
+
+// Components
+import {
+  Skeleton,
+  Divider,
+  Box,
+  Container,
+  Center,
+  Heading,
+  Grid,
+  GridItem,
+  ChakraProvider,
+  Text,
+  Flex,
+  Image,
+  Link
+} from "@chakra-ui/react";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
